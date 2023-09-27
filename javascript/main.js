71886ef0b8763f88e47e11c1db838a9fbd1bf28f
doncity-tech <princewill.opara@outlook.com>
2023-09-27 08:35:23 +0100

    updating the home page

--- javascript/main.js
+++ javascript/main.js
@@ -7,4 +7,15 @@
   qs('.hero-btn').addEventListener('click', () => {
     window.open('./register.html', '_self');
   })
+
+  if (qs('#scroll-btn')) {
+    qs('#scroll-btn').addEventListener('click', () => {
+      window.scrollTo(0, 0);
+    });
+  }
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) { qs('#scroll-btn').style.visibility = 'visible'; }
+    else { qs('#scroll-btn').style.visibility = 'hidden'; }
+  });
 })();
